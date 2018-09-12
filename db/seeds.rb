0d4f87c05bb2db9fc33a345337d011b608e52702
github username <github email address>
2018-09-12 16:44:13 -0400

    seed changest

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,61 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Topic.create(name: 'FamilyGuy', description: Faker::FamilyGuy.quote)
-Topic.create(name: 'Hobbit', description: Faker::Hobbit.quote)
-Topic.create(name: 'HowIMetYourMother', description: Faker::HowIMetYourMother.quote)
-Topic.create(name: 'StarWars', description: Faker::StarWars.quote)
-Topic.create(name: 'GameOfThrones', description: Faker::GameOfThrones.quote)
+Topic.create(name: 'Gods', description: 'How many do YOU know?')
+Topic.create(name: 'Pokemon', description: "Gotta guess 'em all")
+Topic.create(name: 'Tolkein', description: 'From Middle Earth and the tale of the Ring')
+Topic.create(name: 'StarWars', description: 'How well do YOU know the galaxy far far away....')
+Topic.create(name: 'Music', description: "Jamin' with the greats")
 
-10.times do
-  Prompt.create(name: Faker::FamilyGuy.unique.character, topic_id: 1)
+30.times do
+  randnum = Random.new.rand(1...3)
+  if randnum == 1
+    Prompt.create(name: Faker::Ancient.unique.god, topic_id: 1)
+  elsif randnum == 2
+    Prompt.create(name: Faker::Ancient.unique.titan, topic_id: 1)
+  elsif randnum == 3
+    Prompt.create(name: Faker::Ancient.unique.hero, topic_id: 1)
+  # elsif randnum == 4
+  #   Prompt.create(name: Faker::Ancient.unique.primordial, topic_id: 1)
+  end
 end
 
-10.times do
-  Prompt.create(name: Faker::Hobbit.unique.character, topic_id: 2)
+50.times do
+  Prompt.create(name: Faker::Pokemon.name, topic_id: 2)
 end
 
-10.times do
-  Prompt.create(name: Faker::HowIMetYourMother.unique.character, topic_id: 3)
+50.times do
+  randnum = Random.new.rand(1...2)
+  if randnum == 1
+    Prompt.create(name: Faker::LordOfTheRings.unique.character, topic_id: 3)
+  elsif randnum == 2
+    Prompt.create(name: Faker::Hobbit.unique.character, topic_id: 3)
+  # elsif randnum == 3
+  #   Prompt.create(name: Faker::LordOfTheRings.unique.location, topic_id: 3)
+  # elsif randnum == 4
+  #   Prompt.create(name: Faker::Hobbit.unique.location, topic_id: 3)
+  end
 end
 
-10.times do
+50.times do
+  randnum = Random.new.rand(1...2)
+if randnum == 1
   Prompt.create(name: Faker::StarWars.unique.character, topic_id: 4)
+elsif randnum == 2
+    Prompt.create(name: Faker::StarWars.unique.planet, topic_id: 4)
+# elsif randnum == 3
+#     Prompt.create(name: Faker::StarWars.unique.droid, topic_id: 4)
+end
 end
 
-10.times do
-  Prompt.create(name: Faker::GameOfThrones.unique.character, topic_id: 5)
+50.times do
+  randnum = Random.new.rand(1...2)
+  if randnum == 1
+    Prompt.create(name: Faker::Music.unique.band, topic_id: 5)
+  # elsif randnum == 2
+  #   Prompt.create(name: Faker::Music.unique.album, topic_id: 5)
+  elsif randnum == 2
+    Prompt.create(name: Faker::Music.unique.instrument, topic_id: 5)
+  end
+
 end
